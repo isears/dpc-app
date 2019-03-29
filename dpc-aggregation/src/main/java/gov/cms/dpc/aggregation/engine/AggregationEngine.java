ec03771f41e0b3f195829a5578333917af77f6cc
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-03-29 13:28:42 -0400

    DPC-146: Migrated the stream logic to RxJava
    First piece of making the aggregation client more resilient, migrated some of the logic to RxJava, so we can simply flow and observe individual records.

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/AggregationEngine.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/AggregationEngine.java
@@ -10,6 +10,9 @@ import gov.cms.dpc.queue.JobStatus;
 import gov.cms.dpc.queue.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rx.Observable;
+import rx.observables.BlockingObservable;
+import rx.schedulers.Schedulers;
 
 import javax.inject.Inject;
 import java.io.FileOutputStream;
@@ -22,7 +25,7 @@ import java.util.UUID;
 public class AggregationEngine implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(AggregationEngine.class);
-    private static final char delim = '\n';
+    private static final char DELIM = '\n';
 
     private final JobQueue queue;
     private final BlueButtonClient bbclient;
@@ -36,40 +39,14 @@ public class AggregationEngine implements Runnable {
         this.bbclient = bbclient;
         this.context = FhirContext.forDstu3();
         this.exportPath = config.getString("exportPath");
+        logger.error("Creating!!!");
     }
 
     @Override
     public void run() {
 
         while (run) {
-            final Optional<Pair<UUID, Object>> workPair = this.queue.workJob();
-            if (workPair.isEmpty()) {
-                try {
-                    logger.debug("No job, waiting 2 seconds");
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                final JobModel model = (JobModel) workPair.get().getRight();
-                final UUID jobID = workPair.get().getLeft();
-                logger.debug("Has job {}. Working.", jobID);
-                List<String> attributedBeneficiaries = model.getPatients();
-
-                if (!attributedBeneficiaries.isEmpty()) {
-                    logger.debug("Has {} attributed beneficiaries",attributedBeneficiaries.size());
-                    try {
-                        this.workJob(jobID, model);
-                        this.queue.completeJob(jobID, JobStatus.COMPLETED);
-                    } catch (Exception e) {
-                        logger.error("Cannot process job {}", jobID, e);
-                        this.queue.completeJob(jobID, JobStatus.FAILED);
-                    }
-                } else {
-                    logger.error("Cannot execute Job {} with no beneficiaries", jobID);
-                    this.queue.completeJob(jobID, JobStatus.FAILED);
-                }
-            }
+            this.pollQueue();
         }
         logger.info("Shutting down aggregation engine");
     }
@@ -78,23 +55,68 @@ public class AggregationEngine implements Runnable {
         this.run = false;
     }
 
-    private void workJob(UUID jobID, JobModel job) throws IOException {
+    void pollQueue() {
+        final Optional<Pair<UUID, Object>> workPair = this.queue.workJob();
+        if (workPair.isEmpty()) {
+            try {
+                logger.debug("No job, waiting 2 seconds");
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            final JobModel model = (JobModel) workPair.get().getRight();
+            final UUID jobID = workPair.get().getLeft();
+            logger.debug("Has job {}. Working.", jobID);
+            List<String> attributedBeneficiaries = model.getPatients();
+
+            if (!attributedBeneficiaries.isEmpty()) {
+                logger.debug("Has {} attributed beneficiaries", attributedBeneficiaries.size());
+                try {
+                    this.workJob(jobID, model);
+                    this.queue.completeJob(jobID, JobStatus.COMPLETED);
+                } catch (Exception e) {
+                    logger.error("Cannot process job {}", jobID, e);
+                    this.queue.completeJob(jobID, JobStatus.FAILED);
+                }
+            } else {
+                logger.error("Cannot execute Job {} with no beneficiaries", jobID);
+                this.queue.completeJob(jobID, JobStatus.FAILED);
+            }
+        }
+    }
+
+    void workJob(UUID jobID, JobModel job) throws IOException {
         final IParser parser = context.newJsonParser();
+        logger.error("Working");
         try (final FileOutputStream writer = new FileOutputStream(String.format("%s/%s.ndjson", exportPath, jobID.toString()))) {
-            job.getPatients()
-                    .stream()
-                    .map(this.bbclient::requestPatientFromServer)
-                    .map(parser::encodeResourceToString)
+
+            // Map over the patients and fetch them using a separate thread pool
+            final BlockingObservable<String> stringBlockingObservable = Observable.from(job.getPatients())
+                    .flatMap((patient) -> this.handlePatient(patient, parser))
+                    .subscribeOn(Schedulers.io())
+                    .toBlocking();
+
+
+            stringBlockingObservable
                     .forEach(str -> {
                         try {
-                            logger.debug("Writing {} to file", str);
+                            logger.debug("Writing {} to file on thread {}", str, Thread.currentThread().getName());
                             writer.write(str.getBytes(StandardCharsets.UTF_8));
-                            writer.write(delim);
+                            writer.write(DELIM);
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
                     });
             writer.flush();
         }
+    }
+
+    private Observable<String> handlePatient(String patient, IParser parser) {
+        return Observable
+                .just(patient)
+                .doOnNext(p -> logger.debug("Fetching {} on {}", p, Thread.currentThread().getName()))
+                .map(this.bbclient::requestPatientFromServer)
+                .map(parser::encodeResourceToString);
     }
 }
